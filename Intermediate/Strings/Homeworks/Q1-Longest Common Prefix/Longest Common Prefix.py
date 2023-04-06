847f46ac18ccb511026dc088e57e4ed7991096f9
avishek376 <avishekdeb376@gmail.com>
2023-04-06 20:40:53 +0530

    initial commit

--- Intermediate/Strings/Homeworks/Q1-Longest Common Prefix/Longest Common Prefix.py
+++ Intermediate/Strings/Homeworks/Q1-Longest Common Prefix/Longest Common Prefix.py
@@ -4,19 +4,19 @@ class Solution:
     def longestCommonPrefix(self, A):
         lcp = ""
 
-    if len(A) == 0:
+        if len(A) == 0:
+            return lcp
+        minlen = len(A[0])
+        for i in range(len(A)):
+            minlen = min(len(A[i]), minlen)
+
+        i = 0
+        while (i < minlen):
+            currentChar = A[0][i]
+            for j in range(1, len(A)):
+
+                if A[j][i] != currentChar:
+                    return lcp
+            lcp += currentChar
+            i += 1
         return lcp
-    minlen = len(A[0])
-    for i in range(len(A)):
-        minlen = min(len(A[i]), minlen)
-
-    i = 0
-    while (i < minlen):
-        currentChar = A[0][i]
-        for j in range(1, len(A)):
-
-            if A[j][i] != currentChar:
-                return lcp
-        lcp += currentChar
-        i += 1
-    return lcp
