69aea0f9c1b75299395e20a4438caa5b8606ecbb
avishek376 <avishekdeb376@gmail.com>
2023-04-25 23:00:30 +0530

    initial commit

---
+++ Intermediate/Intermediate DSA : Strings/Homework/Q2. Isalnum()/Idea.md
@@ -0,0 +1,5 @@
+Loop through the character array and check if the ascii value of each character lies in the range of an alphabet or number.
+If all the characters are alphanumeric, then return 1, else return 0.
+
+    TC: O(N)
+    SC: O(1)
---
+++ Intermediate/Intermediate DSA : Strings/Homework/Q2. Isalnum()/Isalnum().py
@@ -0,0 +1,25 @@
+class Solution:
+    # @param A : list of characters
+    # @return an integer
+    def solve(self, A):
+        A = ''.join(A)
+        if A.isalnum():
+            return 1
+        else:
+            return 0
+
+    '''
+        class Solution:
+        # @param A : list of characters
+        # @return an integer
+        def solve(self, A):
+            for x in A:
+                if x >= 'a' and x <= 'z':
+                    continue
+                if x >= 'A' and x <= 'Z':
+                    continue
+                if x >= '0' and x <= '9':
+                    continue
+                return 0
+            return 1
+    '''
