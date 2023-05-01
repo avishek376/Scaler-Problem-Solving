adde1ac53a9e25d85b3fc8978fdb33408361c7a4
avishek376 <avishekdeb376@gmail.com>
2023-05-01 23:33:47 +0530

    initial commit

--- Intermediate/06 Intermediate DSA: Arrays : 2D Matrices/Assignment/Q1. Column Sum/Column Sum.py
+++ Intermediate/06 Intermediate DSA: Arrays : 2D Matrices/Assignment/Q1. Column Sum/Column Sum.py
@@ -2,12 +2,12 @@ class Solution:
     # @param A : list of list of integers
     # @return a list of integers
     def solve(self, A):
-        n = len(A)
-        m = len(A[0])
+        rows = len(A)
+        cols = len(A[0])
         lst = []
-        for i in range(m):
+        for i in range(cols):
             summ = 0
-            for j in range(n):
+            for j in range(rows):
                 summ += A[j][i]
             lst.append(summ)
         return lst
