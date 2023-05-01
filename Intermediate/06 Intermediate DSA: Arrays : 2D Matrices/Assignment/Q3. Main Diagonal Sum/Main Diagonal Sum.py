f62615b9a6511e791efb36223155edc77526742f
avishek376 <avishekdeb376@gmail.com>
2023-05-01 23:38:38 +0530

    initial commit

--- Intermediate/06 Intermediate DSA: Arrays : 2D Matrices/Assignment/Q3. Main Diagonal Sum/Main Diagonal Sum.py
+++ Intermediate/06 Intermediate DSA: Arrays : 2D Matrices/Assignment/Q3. Main Diagonal Sum/Main Diagonal Sum.py
@@ -2,6 +2,17 @@ class Solution:
     # @param A : tuple of list of integers
     # @return an integer
     def solve(self, A):
+        N = len(A)
+
+        sum = 0
+
+        for i in range(0, N):
+            # A[i][i] is the main diagonal element
+            sum += A[i][i]
+
+        return sum
+
+    '''def solve(self, A):
         n = len(A)
         summ = 0
 
@@ -11,7 +22,7 @@ class Solution:
             summ += A[r][c]
             r+= 1
             c+=1
-        return summ
+        return summ'''
 
 
     '''
