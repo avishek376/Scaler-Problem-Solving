a93f11c1892dff2f6fc3067f7e7bd90d79c2eedc
avishek376 <avishekdeb376@gmail.com>
2023-05-02 00:48:07 +0530

    initial commit

--- Intermediate/06 Intermediate DSA: Arrays : 2D Matrices/Assignment/Q6. Matrix Transpose/Matrix Transpose.py
+++ Intermediate/06 Intermediate DSA: Arrays : 2D Matrices/Assignment/Q6. Matrix Transpose/Matrix Transpose.py
@@ -2,10 +2,24 @@ class Solution:
     # @param A : list of list of integers
     # @return a list of list of integers
     def solve(self, A):
-        n = len(A)
-        m = len(A[0])
-        res = [[0]*n for k in range(m)]
-        for i in range(n):
-            for j in range(m):
+        rows = len(A)
+        cols = len(A[0])
+        res = [[0]*rows for k in range(cols)]
+        for i in range(rows):
+            for j in range(cols):
                 res[j][i] = A[i][j]
         return res
+
+
+    '''
+    def solve(self, A):
+        ans = []
+        # Iterating over the columns
+        for i in range(0,len(A[0])):
+            temp = []
+            # Iterating over the rows
+            for j in range(0,len(A)):
+                temp.append(A[j][i])
+            ans.append(temp)
+        return ans
+    '''
