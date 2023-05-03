c3065ad72064f504bf0f88c703c5caaed9e6c019
avishek376 <avishekdeb376@gmail.com>
2023-05-03 11:55:46 +0530

    initial commit

--- Intermediate/07 Intermediate DSA: Arrays : Sliding Window/Assignment/Q3. Spiral Order Matrix II/Spiral Order Matrix II.py
+++ Intermediate/07 Intermediate DSA: Arrays : Sliding Window/Assignment/Q3. Spiral Order Matrix II/Spiral Order Matrix II.py
@@ -12,28 +12,28 @@ class Solution:
         num = 1
         while top <= bottom and left <= right:
             if direction == 0:
-                # move right
+                # move right..top fixed
                 for i in range(left, right + 1):
                     ret[top][i] = num
                     num += 1
                 top += 1
                 direction = 1
             elif direction == 1:
-                # move down
+                # move down..right fixed
                 for i in range(top, bottom + 1):
                     ret[i][right] = num
                     num += 1
                 right -= 1
                 direction = 2
             elif direction == 2:
-                # move left
+                # move left..bottom fixed
                 for i in range(right, left - 1, -1):
                     ret[bottom][i] = num
                     num += 1
                 bottom -= 1
                 direction = 3
             else:
-                # move up
+                # move up..left fixed
                 for i in range(bottom, top - 1, -1):
                     ret[i][left] = num
                     num += 1
