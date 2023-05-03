c7621e22025b1669019235a9fbc089809d5fb0df
avishek376 <avishekdeb376@gmail.com>
2023-05-03 18:31:15 +0530

    initial commit

--- Intermediate/08 Intermediate DSA : Arrays: Interview Problems - 1/Homework/Q4. Multiple left rotations of the array/Multiple left rotations of the array.py
+++ Intermediate/08 Intermediate DSA : Arrays: Interview Problems - 1/Homework/Q4. Multiple left rotations of the array/Multiple left rotations of the array.py
@@ -3,10 +3,22 @@ class Solution:
     # @param B : list of integers
     # @return a list of list of integers
     def solve(self, A, B):
+        ans = []
+        for i in range(len(B)):
+            temp = []
+            ind = B[i] % len(A)
+            for j in range(ind, len(A)):
+                temp.append(A[j])
+            for j in range(ind):
+                temp.append(A[j])
+            ans.append(temp);
+
+        return ans
+    '''def solve(self, A, B):
         R = []
         k = len(A)
         for i in range(len(B)):
             x = B[i] % k
             R.append(A[x:] + A[:x])
 
-        return R
+        return R'''
