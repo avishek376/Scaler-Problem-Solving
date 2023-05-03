ba5a65fd527fdea16fd0b42c0363bcfa5ca3cec4
avishek376 <avishekdeb376@gmail.com>
2023-05-03 16:30:19 +0530

    initial commit

--- Intermediate/08 Intermediate DSA : Arrays: Interview Problems - 1/Homework/Q1. Maximum positivity/Maximum positivity.py
+++ Intermediate/08 Intermediate DSA : Arrays: Interview Problems - 1/Homework/Q1. Maximum positivity/Maximum positivity.py
@@ -1,6 +1,21 @@
 class Solution:
     # @param A : list of integers
     # @return a list of integers
+    class Solution:
+        # @param A : list of integers
+        # @return a list of integers
+        def solve(self, A):
+            ret = [[]]
+            for i in A:
+                if i < 0:
+                    ret.append([])
+                else:
+                    ret[-1].append(i)
+            mxlen = max([len(i) for i in ret])
+            for i in ret:
+                if len(i) == mxlen:
+                    return i
+    '''
     def solve(self, A):
         n = len(A)
         ans = 0
@@ -13,3 +28,4 @@ class Solution:
                         temp = A[i:j+1]
                         ans = len(A[i:j+1])
         return temp
+        '''
