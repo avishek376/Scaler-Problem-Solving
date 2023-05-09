0a6c8e826bb1c526b9c61e862f549e269bae99b7
avishek376 <avishekdeb376@gmail.com>
2023-05-09 14:28:15 +0530

    initial commit

--- Intermediate/14 Intermediate DSA : Strings/Assignment/Q1-tolower/tolower().py
+++ Intermediate/14 Intermediate DSA : Strings/Assignment/Q1-tolower/tolower().py
@@ -2,9 +2,17 @@ class Solution:
     # @param A : list of characters
     # @return a list of characters
     def to_lower(self, A):
+        s = ''
+        for c in A:
+            if c >= 'A' and c <= 'Z':
+                s = s + chr((ord(c) + 32))
+            else:
+                s = s + c
+        return s
+    '''def to_lower(self, A):
 
         for i in range(len(A)):
             if A[i].isupper():
                 A[i] = (chr(ord(A[i]) ^ (1 << 5)))
 
-        return A
+        return A'''
