e548f9ddc4652bdb50c6c60ec028a0abbf8dbefb
avishek376 <avishekdeb376@gmail.com>
2023-05-09 10:57:37 +0530

    initial commit

--- Intermediate/12 Intermediate DSA: Maths - Modular Arthimetic Introduction/Homework/Q1. Leap year? - III/Leap year? - III.py
+++ Intermediate/12 Intermediate DSA: Maths - Modular Arthimetic Introduction/Homework/Q1. Leap year? - III/Leap year? - III.py
@@ -2,11 +2,9 @@ class Solution:
     # @param A : integer
     # @return an integer
     def solve(self, A):
-        temp = (A%100)
-        if A % 400 == 0:
+        if A % 4 == 0 and A % 100 != 0:
             return 1
-        elif A % 100 == 0:
-            return 0
-        elif temp % 4 == 0:
+        elif A % 400 == 0:
             return 1
+
         return 0
