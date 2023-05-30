81d6c527df1fb8ac880215a80d42dd3d6a2a7eb0
avishek376 <avishekdeb376@gmail.com>
2023-05-30 13:33:43 +0530

    initial commit

--- Intermediate/18 Intermediate DSA : Recursion - 2/Homework/Q2. Kth Symbol - Easy/Kth Symbol - Easy.py
+++ Intermediate/18 Intermediate DSA : Recursion - 2/Homework/Q2. Kth Symbol - Easy/Kth Symbol - Easy.py
@@ -7,7 +7,9 @@ class Solution:
     def solve(self, A, B):
         if B == 0:
             return 0
+        # by this we can find the parent
         val = self.solve(A-1,B//2)
+        #if you observe the even indexed childs are same as parent and odds are compliment
         if B % 2 == 0:
             return val
         else:
