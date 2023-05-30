7fb526341f95591094a4c7d3b8d577a9ab1da194
avishek376 <avishekdeb376@gmail.com>
2023-05-30 13:17:15 +0530

    initial commit

--- Intermediate/18 Intermediate DSA : Recursion - 2/Assignment/Q1. Implement Power Function/Implement Power Function.py
+++ Intermediate/18 Intermediate DSA : Recursion - 2/Assignment/Q1. Implement Power Function/Implement Power Function.py
@@ -8,6 +8,26 @@ class Solution:
     def pow(self, A, B, C):
         def mypow(a,n,m):
             if n == 0:
+                return 1 % m
+            elif n % 2 == 0:
+                temp = mypow(a, n//2, m)
+                return (temp * temp) % m
+            else:
+                temp = mypow(a, n-1, m)
+                return (a * temp) % m
+
+        return mypow(A,B,C)
+
+'''import sys
+sys.setrecursionlimit(10**6)
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @param C : integer
+    # @return an integer
+    def pow(self, A, B, C):
+        def mypow(a,n,m):
+            if n == 0:
                 return 1%m
             p = mypow(a,n//2,m)
             if n % 2 == 0:
@@ -15,5 +35,5 @@ class Solution:
             else:
                 return ((p*p) % m * a) % m
 
-        return mypow(A,B,C)
+        return mypow(A,B,C)'''
 
