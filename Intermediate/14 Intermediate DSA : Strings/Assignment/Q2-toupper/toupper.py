96878262b559d6fa68cd5fa60b2e82e066746dd9
avishek376 <avishekdeb376@gmail.com>
2023-06-03 00:42:58 +0530

    initial commit

--- Intermediate/14 Intermediate DSA : Strings/Assignment/Q2-toupper/toupper.py
+++ Intermediate/14 Intermediate DSA : Strings/Assignment/Q2-toupper/toupper.py
@@ -2,7 +2,20 @@ class Solution:
     # @param A : list of characters
     # @return a list of characters
     def to_upper(self, A):
+        s = ''
+        for c in A:
+            if c >= 'a' and c <= 'z':
+                s = s + chr((ord(c) - 32))
+            else:
+                s = s + c
+        return s
+
+'''class Solution:
+    # @param A : list of characters
+    # @return a list of characters
+    def to_upper(self, A):
         for i in range(len(A)):
             if A[i].islower():
                 A[i] = chr(ord(A[i])^(1<<5))
         return A
+'''
