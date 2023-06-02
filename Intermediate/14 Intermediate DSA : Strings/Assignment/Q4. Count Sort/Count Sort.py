2357c51ca240998bb48037527f310ee05e10284b
avishek376 <avishekdeb376@gmail.com>
2023-06-02 23:40:04 +0530

    initial commit

--- Intermediate/14 Intermediate DSA : Strings/Assignment/Q4. Count Sort/Count Sort.py
+++ Intermediate/14 Intermediate DSA : Strings/Assignment/Q4. Count Sort/Count Sort.py
@@ -2,6 +2,26 @@ class Solution:
     # @param A : list of integers
     # @return a list of integers
     def solve(self, A):
+                                      # for this I used an index array upto max elemnt of A not 10**5 to save time
+        max1=float("-inf")
+        for i in A:
+            if i >max1:
+                max1=i
+
+        index=[0]*(max1+1)           # created an index array starting from 0 upto max elemnt as end
+        for i in A:
+            index[i]+=1              # here whatever be elemnt of A its index  will get its total count\
+
+        ans=[]
+        for i in range(max1+1):       # picks elemnt one by one
+            ans+=index[i]*[i]         # in ans array the value of index * single elemnt array =[el,el,el,...(upto its value in index)]
+        return ans
+
+
+'''class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def solve(self, A):
 
         mx = max(A)
         freq = [0 for j in range(mx + 1)]
@@ -11,4 +31,4 @@ class Solution:
         for i in range(mx + 1):
             for j in range(freq[i]):
                 ans.append(i)
-        return ans
+        return ans'''
--- Intermediate/14 Intermediate DSA : Strings/Assignment/Q4. Count Sort/Idea.md
+++ Intermediate/14 Intermediate DSA : Strings/Assignment/Q4. Count Sort/Idea.md
@@ -3,7 +3,25 @@ elements of the array using a frequency array. Then
 we will move from 1 to size of the the frequency array
 and insert them in our final sorted array.
 
+
+
     TC : O(N + K)
     SC : O(K)
     where N is the size and K is the maximum 
     value of the given array
+
+
+**Approach-2**
+
+ 1)for this I used an index array upto max elemnt of A not 10**5 to save time
+              
+ 2)created an index array starting from 0 upto max elemnt as end
+ 3)here whatever be elemnt of A its index  will get its total count\
+ 4)picks elemnt one by one
+ 5)in ans array the value of index * single elemnt array =[el,el,el,...(upto its value in index)]
+
+    The given solution is based on this approach
+
+    TC: O(N+K)
+    SC: O(K)
+
