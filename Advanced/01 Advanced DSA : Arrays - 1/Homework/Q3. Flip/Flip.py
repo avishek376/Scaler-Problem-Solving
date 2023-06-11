c3b2b97c930ba7325ea47a0af0ce269598764e03
avishek376 <avishekdeb376@gmail.com>
2023-06-11 18:59:57 +0530

    initial commit

--- Advanced/01 Advanced DSA : Arrays - 1/Homework/Q3. Flip/Flip.py
+++ Advanced/01 Advanced DSA : Arrays - 1/Homework/Q3. Flip/Flip.py
@@ -2,6 +2,24 @@ class Solution:
     # @param A : string
     # @return a list of integers
     def flip(self, A):
+        a = [-1 if i == '1' else 1 for i in A]
+        csum, bsum, s, e, idx = 0, 0, 0, 0, 0
+        for i in range(len(a)):
+            csum += a[i]
+            if csum < 0:
+                csum, idx = 0, i + 1
+            if csum > bsum:
+                bsum, s, e = csum, idx, i
+
+        if bsum == 0:
+            return []
+        return [s + 1, e + 1]
+
+
+'''class Solution:
+    # @param A : string
+    # @return a list of integers
+    def flip(self, A):
         cur = 0
         maxx = 0
         l = 0
@@ -29,4 +47,4 @@ class Solution:
         if (maxx == 0):
             return []
         else:
-            return ans
+            return ans'''
