a4f267fbcbd3c8b5d97e14ef3a8c95bf389c52b7
avishek376 <avishekdeb376@gmail.com>
2023-06-12 01:09:26 +0530

    initial commit

--- Advanced/03 Advanced DSA : Arrays - 3/Assignment/Q2. First Missing Integer/First Missing Integer.py
+++ Advanced/03 Advanced DSA : Arrays - 3/Assignment/Q2. First Missing Integer/First Missing Integer.py
@@ -5,14 +5,17 @@ class Solution:
         n = len(A)
         i = 0
         while i<n:
+            # if the value is out of range or already in the correct place then move forward
             if A[i]<1 or A[i]>n or i == A[i]-1:
                 i += 1
             else:
+                # find the actual index of the value in 0 based indexing.
                 indx = A[i]-1
+                # for duplicate scenarios. If the already one instance of the value is not present then swap else move forward
                 if A[indx] == A[i]:
                     i += 1
                 else:
-                    A[i],A[indx] = A[indx],A[i]
+                    A[i], A[indx] = A[indx], A[i]
         for i in range(n):
             if i != A[i]-1:
                 return i+1
--- Advanced/03 Advanced DSA : Arrays - 3/Assignment/Q2. First Missing Integer/Idea.md
+++ Advanced/03 Advanced DSA : Arrays - 3/Assignment/Q2. First Missing Integer/Idea.md
@@ -2,6 +2,11 @@
 
 ![img.png](img.png)
 
+The idea is to sort the in range natural numbers [1,n] to their correct position in 0 based indexing.
+Then check the array/arraylist which does not match the position. The natural number which does not
+match the position is the answer. If all the elements are in correct position then the max + 1 is The
+answer.
+
 
 
     TC : O(N)
