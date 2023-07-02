57bf05baf5a07a5e2fbc71ea18fa1696888566b7
avishek376 <avishekdeb376@gmail.com>
2023-07-02 16:03:35 +0530

    initial commit

--- Advanced/02 Advanced DSA : Arrays - 2 /Assignment/Q3. Search in a row wise and column wise sorted matrix/Search in a row wise and column wise sorted matrix.py
+++ Advanced/02 Advanced DSA : Arrays - 2 /Assignment/Q3. Search in a row wise and column wise sorted matrix/Search in a row wise and column wise sorted matrix.py
@@ -4,14 +4,14 @@ class Solution:
     # @return an integer
     def solve(self, A, B):
 
-        n = len(A)
-        m = len(A[0])
+        rows = len(A)
+        cols = len(A[0])
         # set indexes for top right element
         i = 0
-        j = m - 1
+        j = cols - 1
         mini = 0
         ans = 10 ** 8
-        while (i < n and j >= 0):
+        while (i < rows and j >= 0):
             if (A[i][j] == B):
                 mini = (i + 1) * 1009 + (j + 1)
                 ans = min(mini, ans)
