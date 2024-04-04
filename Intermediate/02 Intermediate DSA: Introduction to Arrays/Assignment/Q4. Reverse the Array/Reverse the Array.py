667a7efed729636aecd85baa668a47c1d7b38e90
avishek376 <avishekdeb376@gmail.com>
2024-04-04 14:14:24 +0530

    initial commit

--- Intermediate/02 Intermediate DSA: Introduction to Arrays/Assignment/Q4. Reverse the Array/Reverse the Array.py
+++ Intermediate/02 Intermediate DSA: Introduction to Arrays/Assignment/Q4. Reverse the Array/Reverse the Array.py
@@ -3,10 +3,24 @@ class Solution:
     # @return a list of integers
     def solve(self, A):
         lst = []
-        for i in range(len(A)-1, -1, -1):
+        for i in range(len(A) - 1, -1, -1):
             lst.append(A[i])
         return lst
 
+    '''
+        if A is a list instead of tuple and we don't
+        have to return a different array instead of same array
+        def solve(A):
+            i = 0
+            j = len(A)-1
+            while i<j:
+                A[i],A[j] = A[j],A[i]
+                i += 1
+                j -= 1
+        
+            return A
+    
+    '''
     '''
         def solve(A):
             lst = [0]*len(A)
@@ -14,6 +28,3 @@ class Solution:
                 lst[item] = A[len(A)-item-1]
             return lst
     '''
-
-
-
