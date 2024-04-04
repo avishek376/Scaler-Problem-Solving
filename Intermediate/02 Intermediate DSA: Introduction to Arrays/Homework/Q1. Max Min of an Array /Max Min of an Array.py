c8e072b7b789bada177772f6d2e9069eccbcd4cc
avishek376 <avishekdeb376@gmail.com>
2024-04-04 14:39:33 +0530

    initial commit

--- Intermediate/02 Intermediate DSA: Introduction to Arrays/Homework/Q1. Max Min of an Array /Max Min of an Array.py
+++ Intermediate/02 Intermediate DSA: Introduction to Arrays/Homework/Q1. Max Min of an Array /Max Min of an Array.py
@@ -4,10 +4,23 @@ class Solution:
     def solve(self, A):
         mx = A[0]
         mn = A[-1]
-        for i in range(1,len(A)):
+        for i in range(1, len(A)):
             if A[i] > mx:
                 mx = A[i]
-        for i in range(len(A)-1):
-            if A[i]<mn:
+        for i in range(len(A) - 1):
+            if A[i] < mn:
                 mn = A[i]
         return mx + mn
+
+    '''
+        def solve(self, A):
+            mx = A[0]
+            mn = A[-1]
+            for i in range(1,len(A)):
+                if A[i]>mx:
+                    mx = A[i]
+            for i in range(len(A)-2,-1,-1):
+                if mn > A[i]:
+                    mn = A[i]
+            return mx+mn
+    '''
