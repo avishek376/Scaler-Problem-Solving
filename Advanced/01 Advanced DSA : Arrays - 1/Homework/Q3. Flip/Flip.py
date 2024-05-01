d06c58ac92ba579ee270decbc78879ce01c6a1a4
avishek376 <avishekdeb376@gmail.com>
2024-05-01 18:00:07 +0530

    initial commit

--- Advanced/01 Advanced DSA : Arrays - 1/Homework/Q3. Flip/Flip.py
+++ Advanced/01 Advanced DSA : Arrays - 1/Homework/Q3. Flip/Flip.py
@@ -3,17 +3,17 @@ class Solution:
     # @return a list of integers
     def flip(self, A):
         a = [-1 if i == '1' else 1 for i in A]
-        csum, bsum, s, e, idx = 0, 0, 0, 0, 0
+        csum, bsum, start, end, idx = 0, 0, 0, 0, 0
         for i in range(len(a)):
             csum += a[i]
             if csum < 0:
                 csum, idx = 0, i + 1
             if csum > bsum:
-                bsum, s, e = csum, idx, i
+                bsum, start, end = csum, idx, i
 
         if bsum == 0:
             return []
-        return [s + 1, e + 1]
+        return [start + 1, end + 1]
 
 
 '''class Solution:
