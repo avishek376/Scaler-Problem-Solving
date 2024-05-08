bff0f251dc5b7ba05a13c4c87859af29ff3c6422
avishek376 <avishekdeb376@gmail.com>
2024-05-08 13:18:48 +0530

    initial commit

--- Advanced/02 Advanced DSA : Arrays - 2 /Homework/Q1. Maximum Sum Submatrix/Maximum Sum Submatrix.py
+++ Advanced/02 Advanced DSA : Arrays - 2 /Homework/Q1. Maximum Sum Submatrix/Maximum Sum Submatrix.py
@@ -1,3 +1,43 @@
+def kadane(v):
+    currSum = 0
+    maxSum = -1e9
+    for i in range(len(v)):
+        currSum += v[i]
+        maxSum = max(maxSum, currSum)
+        if currSum < 0: currSum = 0
+    return maxSum
+
+
+class Solution:
+    # @param A : list of list of integers
+    # @return an integer
+    def solve(self, A):
+        r = len(A)
+        c = len(A[0])
+        prefix = [[0 for i in range(c)] for j in range(r)]
+        for i in range(r):
+            for j in range(c):
+                if j == 0:
+                    prefix[i][j] = A[i][j]
+                else:
+                    prefix[i][j] = prefix[i][j - 1] + A[i][j]
+        maxSum = -1e9
+        for i in range(c):
+            for j in range(i, c):
+                v = []
+                for k in range(r):
+                    el = 0
+                    if i == 0:
+                        el = prefix[k][j]
+                    else:
+                        el = prefix[k][j] - prefix[k][i - 1]
+                    v.append(el)
+                maxSum = max(maxSum, kadane(v))
+        return maxSum
+
+
+'''
+
 class Solution:
     # @param A : list of list of integers
     # @return an integer
@@ -26,3 +66,4 @@ class Solution:
                 maxi = max(maxi, currMax)
 
         return maxi
+'''
