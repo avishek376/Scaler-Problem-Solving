4e89480c59bece28d4ebbc0b282cdec37cc3e857
avishek376 <avishekdeb376@gmail.com>
2024-05-18 23:17:00 +0530

    initial commit

--- Advanced/12 Advanced DSA : Sorting - 2/Assignment/Q3. Sum the Difference /Sum the Difference.py
+++ Advanced/12 Advanced DSA : Sorting - 2/Assignment/Q3. Sum the Difference /Sum the Difference.py
@@ -1,4 +1,25 @@
 class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, A):
+        mod = 1000000007
+        N = len(A)
+        A.sort()
+        ans = 0
+        mi = 0#sum of min.m values
+        mx = 0#sum of max.m values
+        for i in range(N):
+
+            mi+=(A[i]%mod *  2**(N-1-i)%mod)  %mod # for smaller elemnts
+
+            mx+=(A[N-1-i]%mod *  2**(N-1-i)%mod)   %mod # for larger elemnts
+
+        ans=(mx-mi)%mod
+
+        return ans
+
+
+'''class Solution:
 	# @param A : list of integers
 	# @return an integer
 	def solve(self, A):
@@ -12,4 +33,4 @@ class Solution:
         ans = 0
         for i in range(n):
             ans+= A[i] * (calc_pow(i) - calc_pow(n-i-1))
-        return ans%mod
+        return ans%mod'''
