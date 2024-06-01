5429549b1af0957433eaf87bba5cbab80f16aa92
avishek376 <avishekdeb376@gmail.com>
2024-06-02 01:13:44 +0530

    Initial commit

--- Intermediate/02 Intermediate DSA: Introduction to Arrays/Homework/Q3. Second Largest /Idea.md
+++ Intermediate/02 Intermediate DSA: Introduction to Arrays/Homework/Q3. Second Largest /Idea.md
@@ -2,5 +2,6 @@ First, we iterate over each element in the list and find the largest element. Le
 Then, we again iterate over each element in the list using a for loop and find the largest element, but we also check if that element is equal to max_elem, if it is we skip it.
 
 
-    TC: O(NlogN)
+    TC: O(N)
+    If you do sorting then it will take O(NlogN)
     SC: O(1)
--- Intermediate/02 Intermediate DSA: Introduction to Arrays/Homework/Q3. Second Largest /Second Largest.py
+++ Intermediate/02 Intermediate DSA: Introduction to Arrays/Homework/Q3. Second Largest /Second Largest.py
@@ -1,3 +1,4 @@
+'''
 class Solution:
     # @param A : list of integers
     # @return an integer
@@ -7,3 +8,30 @@ class Solution:
         if len(l) == 1:
             return -1
         return l[-2]
+
+TC: O(NlogN)
+SC: O(1)
+'''
+
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, A):
+        if len(A) == 1:
+            return -1
+        maxEle = -1
+        for i in A:
+            if maxEle < i:
+                maxEle = i
+
+        secondMax = -1
+        for i in range(len(A)):
+            if A[i] == maxEle:
+                continue
+            elif secondMax < A[i]:
+                secondMax = A[i]
+
+        return secondMax
+
+# TC: O(N)
+# SC: O(1)
