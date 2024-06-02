6769f598cd33310f06b118dcaf03c166f7b4d5cb
avishek376 <avishekdeb376@gmail.com>
2024-06-02 15:48:52 +0530

    Initial commit

--- Intermediate/04 Intermediate DSA: Arrays - Carry Forward/Homework/Q3. Amazing Subarrays/Amazing Subarrays.py
+++ Intermediate/04 Intermediate DSA: Arrays - Carry Forward/Homework/Q3. Amazing Subarrays/Amazing Subarrays.py
@@ -10,3 +10,27 @@ class Solution:
                 count += n - i
         return count % 10003
 
+
+'''
+class Solution:
+    # @param A : string
+    # @return an integer
+    def solve(self, A):
+        mySet = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+
+        i = len(A)-1
+        count = 0
+        result = 0
+        while i>=0:
+            if A[i] in mySet:
+                count+=1
+                result += count
+            else:
+                count+=1
+            i-=1
+        return result%10003
+        
+        TC: O(N)
+        SC: O(1)
+'''
+
--- Intermediate/04 Intermediate DSA: Arrays - Carry Forward/Homework/Q7. Bulbs/Idea.md
+++ Intermediate/04 Intermediate DSA: Arrays - Carry Forward/Homework/Q7. Bulbs/Idea.md
@@ -12,6 +12,15 @@ Case 2:
     Press switch 1 : [0 0 1 0]
     Press switch 0 : [1 1 0 1]  
 Therefore we can choose a particular order. To make things easier, let’s go from left to right. At the current position, if the bulb is on, we move to the right, else we switch it on. This works because changing any switch to the right of it will not affect it anymore.
-    
+
+Idea-1
+![img.jpg](img.jpg)
+
+    TC: O(N)
+    SC: O(1)
+
+Idea-2
+![img1.jpg](img1.jpg)
+
     TC: O(N)
     SC: O(1)
