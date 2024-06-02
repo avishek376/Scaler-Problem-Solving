508a7f629b4b9e8147586541137ab059ecd3f7a4
avishek376 <avishekdeb376@gmail.com>
2024-06-02 15:52:44 +0530

    Initial commit

--- Intermediate/04 Intermediate DSA: Arrays - Carry Forward/Homework/Q7. Bulbs/Bulbs.py
+++ Intermediate/04 Intermediate DSA: Arrays - Carry Forward/Homework/Q7. Bulbs/Bulbs.py
@@ -4,14 +4,31 @@ class Solution:
     def bulbs(self, A):
         switchCount = 0
 
-    for b in A:
-        if switchCount % 2 == 0:
-            b = b
-        else:
-            b = 1 - b
+        for b in A:
+            if switchCount % 2 == 0:
+                b = b
+            else:
+                b = 1 - b
 
-        if b == 1:
-            continue
-        else:
-            switchCount += 1
-    return switchCount
+            if b == 1:
+                continue
+            else:
+                switchCount += 1
+
+        return switchCount
+
+'''
+
+class Solution:
+	# @param A : list of integers
+	# @return an integer
+	def bulbs(self, A):
+        state = 0
+        switchCount = 0
+
+        for i in range(len(A)):
+            if A[i] == state:
+                switchCount += 1
+                state = 1-state
+        return switchCount
+'''
