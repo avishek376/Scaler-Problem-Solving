442bd99a99ddd44104e626d1cbd8127bd4fded64
avishek376 <avishekdeb376@gmail.com>
2024-07-01 15:19:59 +0530

    Initial commit

--- Advanced/39 Advanced DSA: DP 2 /Assignment/Q2. Unique Paths in a Grid/Unique Paths in a Grid.py
+++ Advanced/39 Advanced DSA: DP 2 /Assignment/Q2. Unique Paths in a Grid/Unique Paths in a Grid.py
@@ -29,3 +29,40 @@ class Solution:
 
         uniquePath(A, dpArr, rows - 1, cols - 1)
         return dpArr[rows - 1][cols - 1]
+
+
+'''
+class Solution:
+	# @param A : list of list of integers
+	# @return an integer
+	def uniquePathsWithObstacles(self, A):
+        rows = len(A)
+        cols = len(A[0])
+
+        dpArr = [[-1] * cols for _ in range(rows)]
+
+        
+        def uniquePath(mat,dpArr,r,c):
+            if r < 0 or c < 0:
+                return 0
+
+            if r == 0 and c == 0 and mat[r][c] == 0:
+                return 1            
+            elif r == 0 and c == 0 and mat[r][c] == 1:
+                return 0
+
+            if mat[r][c] == 1:
+                return 0
+            if dpArr[r][c] != -1:
+                return dpArr[r][c]
+
+            a = uniquePath(mat,dpArr,r-1,c)
+            b = uniquePath(mat,dpArr,r,c-1)
+
+            dpArr[r][c] = a+b
+            return a+b
+
+        return uniquePath(A,dpArr,rows-1,cols-1) 
+
+
+'''
